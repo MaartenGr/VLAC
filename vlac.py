dd12ba786f0c0b89f6a65ca8bf4725de885e2107
Maarten Grootendorst <maarten_grootendorst@hotmail.com>
2019-04-16 15:19:35 +0200

    Add files via upload

--- vlac.py
+++ vlac.py
@@ -1,9 +1,24 @@
 import numpy as np
 from sklearn.cluster import KMeans
 from sklearn import preprocessing
+# from nltk.cluster import KMeansClusterer
 
 
 class VLAC:
+    """ Convert a collection of text documents to a matrix based on clustered word embeddings
+
+    Vectors of Locally Aggregated Concepts (VLAC) is a method that leverages clusters of word
+    embeddings (i.e., concepts) to create features from a collection of documents.
+
+    k-Means is used to cluster word embeddings into k word clusters, which are named concepts
+    by the similarity of words in a word cluster. For each document, the element-wise sum of
+    residuals of each word embedding to its assigned cluster center is calculated.
+
+    This results in k features vectors, one for each concepts, and all the same size as the
+    dimensions of the word embeddings. All feature vectors are then concatenated, power normalized,
+    and finally, l2 normalization is applied. For example, if 10 concepts were to be created out of
+    word embeddings of size 300 then the resulting document vector would contain 10 x 300 values.
+    """
 
     def __init__(self, documents, model, oov=False):
 
@@ -19,6 +34,24 @@ class VLAC:
             self.dim = model.vector_size
 
     def fit_transform(self, num_concepts):
+        """ Creates concepts (i.e., word clusters) and based on those concepts
+        create a feature space from the concatenated sum of residuals in each
+        concept.
+
+        Parameters
+        ----------
+        num_concepts : int
+            Number of concepts (i.e., word clusters) to be created using a
+            spherical kmeans clustering task
+
+        Returns
+        -------
+        vlac : numpy array
+            A numpy matrix with dimensions (k x vector dimensions) x (number of documents)
+
+        kmeans : sklearn.cluster.k_means_.KMeans
+            A fitted sklearn kmeans model
+        """
         word_vectors = np.array([self.model[word] for word in self.model_word_list])
 
         word_to_concept, concept_centers, kmeans = self._create_clusters(word_vectors,
@@ -32,6 +65,18 @@ class VLAC:
         return vlac, kmeans
 
     def transform(self, kmeans):
+        """ From a fitted kmeans transform a collection of documents to VLAC features
+
+        Parameters
+        ----------
+        kmeans : sklearn.cluster.k_means_.KMeans
+            A fitted sklearn kmeans model
+
+        Returns
+        -------
+        vlac : numpy array
+            A numpy matrix with dimensions (k x vector dimensions) x (number of documents)
+        """
         # Get all words in the document that also appear in the model
         # It does not check it if FastText is used which can handle out-of-vocabulary words
         if self.oov:
@@ -63,10 +108,21 @@ class VLAC:
         on all those words or take the pre-trained model and only select
         words that appear in your documents.
 
+        Parameters
+        ----------
+        word_vectors : numpy array
+            Array of word embeddings of all words in the word_list
+            Indices between word_vectors and word_list match for easier lookup
+
+        word_list : array
+            Array of strings containing the names of words in the word_vectors
+
+        num_concepts : int
+            Number of word clusters (i.e., concepts) to be created
+
         """
         # normalizing results in spherical kmeans
         normalized_vectors = preprocessing.normalize(word_vectors)
-
         kmeans = KMeans(n_clusters=num_concepts)
         kmeans.fit(normalized_vectors)
 
@@ -76,7 +132,32 @@ class VLAC:
         return word_to_concept, concept_centers, kmeans
 
     def _create_vlac_features(self, word_to_concept, residuals, num_concepts):
-        boc_matrix = []
+        """ Transform documents to VLAc features
+
+        For each document, the element-wise sum of residuals of each word embedding to its
+        assigned cluster center is calculated.  This results in k features vectors,
+        one for each concepts, and all the same size as the dimensions of the word embeddings.
+        All feature vectors are then concatenated, power normalized, and finally, l2 normalization
+        is applied. For example, if 10 concepts were to be created out of
+        word embeddings of size 300 then the resulting document vector would contain 10 x 300 values.
+
+        Parameters
+        ----------
+        word_to_concept : dict
+            Dictionary containing word (key) and concept (value) for quick lookup
+
+        residuals : dict
+            Dictionary containing word (key) and word embedding (value) for quick lookup
+
+        num_concepts : int
+            Number of word clusters (i.e., concepts) that were created
+
+        Returns
+        -------
+        vlac : numpy array
+            Matrix containing vlac features for each document
+        """
+        vlac = []
 
         for line in self.documents:
             document_vector = np.zeros([num_concepts, self.dim])
@@ -97,6 +178,6 @@ class VLAC:
             if l2_norm != 0:
                 document_vector = np.divide(document_vector, l2_norm)
 
-            boc_matrix.append(document_vector)
+            vlac.append(document_vector)
 
-        return np.array(boc_matrix)
+        return np.array(vlac)
