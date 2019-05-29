911931b6a2168b66d52633ab1b428e8016513673
Grootendorst <m.p.grootendorst@student.tue.nl>
2019-05-29 11:39:50 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
-from setuptools import setup
+import setuptools
 
-setup(
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
     name='Vectors of Locally Aggregate Concepts',
-    url='',
     author='Maarten Grootendorst',
     author_email='maartengrootendorst@gmail.com',
+    url="https://github.com/MaartenGr/VLAC",
     packages=['vlac'],
     description='Tool for creating document features',
     install_requires=['numpy', 'sklearn'],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     version='0.1',
     license='MIT',
 )
